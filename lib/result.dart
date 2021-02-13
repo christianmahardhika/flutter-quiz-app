3f6aa3b7badce844442fb4bf78aa5a02f8b80a29
Christian <christian.mahardhika@gmail.com>
2021-02-13 17:02:15 +0700

    init project

---
+++ lib/result.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'You did it! Your Total Score is ${resultScore}',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          )
+        ],
+      ),
+    );
+  }
+}
